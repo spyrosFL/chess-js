ec393bb1875da1697d5a96c549d851ea7fb1d659
spyrosFL <spyrosfelekidis@gmail.com>
2024-09-18 19:44:28 +0300

    chess

---
+++ js/app.js
@@ -0,0 +1,150 @@
+const gameBoard = document.querySelector("#gameboard")
+const playerDisplay = document.querySelector("#player")
+const infoDisplay = document.querySelector("#info-display")
+
+//Width of the chessboard
+const width = 8
+
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
+
+//Starting formation of pawns on the chessboard
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+]
+
+function createBoard() {
+    startPieces.forEach((startPieces, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.innerHTML = startPieces
+        square.firstChild?.setAttribute('draggable', true)
+        square.setAttribute('square-id', i)
+
+        // Adding color to the squares
+        const row = Math.floor( (63 - i) / 8) + 1
+        if (row % 2 === 0){
+            square.classList.add(i % 2 === 0 ? 'beige' : 'brown')
+        }else {
+            square.classList.add(i % 2 === 0 ? 'brown' : 'beige')
+        }
+
+        // Adding color to the pieces svg 
+        if ( i <= 15 ) {
+            square.firstChild.firstChild.classList.add('black')
+        }
+
+        if ( i >= 48 ) {
+            square.firstChild.firstChild.classList.add('white')
+        }
+
+        gameBoard.append(square)
+    })
+}
+
+createBoard()
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId
+let draggedElement
+
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+    e.stopPropagation()
+    const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+    const taken = e.target.classList.contains('piece')
+    const valid = checkIfValid(e.target)
+    const opponentGo = playerGo === 'white' ? 'black' : 'white'
+    const takenByOpponent = e.target.firstChild?.classList.contains('opponentGo')
+
+    if (correctGo) {
+        if (takenByOpponent && valid) {
+            e.target.parentNode.append(draggedElement)
+            e.target.remove()
+            changePlayer()
+            return
+        }
+        if (taken && !takenByOpponent){
+            return
+        }
+        if (valid) {
+            e.target.append(draggedElement)
+            changePlayer()
+            return
+        }
+    }
+    //e.target.parentNode.append(draggedElement)//
+    
+    changePlayer()
+}
+
+function checkIfValid(target) {
+    const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'))
+    const startId = Number(startPositionId)
+    const piece = draggedElement.id
+    console.log('targetId', targetId)
+    console.log('startId', startId)
+    console.log('piece', piece)
+
+    switch(piece) {
+        case 'pawn':
+            const starterRow = [8,9,10,11,12,13,14,15]
+            if (starterRow.includes(startId) && startId + width * 2 === targetId && !document.querySelector(`[square-id="${startId + width * 2}"]`).firstChild ||
+            startId + width === targetId && !document.querySelector(`[square-id="${startId + width}"]`).firstChild ||
+            startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild ||
+            startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild 
+            ){
+                return true
+            }
+            break;
+    }
+}
+
+
+
+   function changePlayer(){
+       if (playerGo === 'black') {
+           reverseIds()
+           playerGo = 'white'
+           playerDisplay.textContent = 'white'
+       }else {
+           revertIds()
+           playerGo = 'black'
+           playerDisplay.textContent = 'black'
+       }
+   }
+
+   function reverseIds(){
+       const allSquares = document.querySelectorAll('.square')
+       allSquares.forEach((square, i) => 
+           square.setAttribute('square-id', (width * width - 1) - i))
+   }
+
+   function revertIds() {
+       const allSquares = document.querySelectorAll('.square')
+       allSquares.forEach((square, i) => square.setAttribute('square-id', i))
+   }
+
+
